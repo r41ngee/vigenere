b1192a02514184add96be119ff1ea26f4f798961
r41ngee <r41ngee@yandex.ru>
2025-05-12 15:07:13 +0300

    Удален скрипт из main.py и добавлен новый файл vigenere.py с реализацией шифра Виженера и определением алфавита.

---
+++ vigenere/vigenere.py
@@ -0,0 +1,25 @@
+from fill import word_fill
+
+ALPHABET = [chr(i).lower() for i in range(97, 123)]
+
+
+def cypher(word: str, key: str) -> str:
+    word = word.lower()
+    key = key.lower()
+
+    if len(word) > len(key):
+        key = word_fill(key, len(word))
+
+    result = ""
+
+    for i in range(len(word)):
+        key_lit = key[i]
+        word_lit = word[i]
+
+        result += ALPHABET[(ALPHABET.index(key_lit) + ALPHABET.index(word_lit)) % 25]
+
+    return result
+
+
+if __name__ == "__main__":
+    print(cypher("ATTACKATDAWN", "LEMON"))
