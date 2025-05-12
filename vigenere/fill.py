cd906a8d8ff5cc1e314338380f8a5f9f5a0a51fa
r41ngee <r41ngee@yandex.ru>
2025-05-12 14:53:01 +0300

    Добавлено расширение функции word_fill для генерации ключа до заданного размера в файле fill.py

--- vigenere/fill.py
+++ vigenere/fill.py
@@ -3,3 +3,13 @@ def word_fill(key: str, word_size: int) -> str:
         return key[:word_size]
     elif len(key) == word_size:
         return key
+    else:
+        tempkey = key
+        for i in range(len(key), word_size):
+            tempkey += key[i % len(key)]
+
+        return tempkey
+
+
+if __name__ == "__main__":
+    print(word_fill("lemon", len("lemon")))
